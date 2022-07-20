03dd26cae37bec5e0b60b3bcb2bc16b8dfecb62d
ValeriaTuaeva <valeriya.tuaeva@gmail.com>
2022-07-20 20:57:49 +0300

    updated

--- Lesson 3/route.rb
+++ Lesson 3/route.rb
@@ -3,26 +3,26 @@ class Route
 	
 	attr_reader :first_station
 
-# Имеет начальную и конечную станцию, а также список промежуточных станций. 
-# Начальная и конечная станции указываются при создании маршрута, а промежуточные могут добавляться между ними.
+	# Имеет начальную и конечную станцию, а также список промежуточных станций. 
+	# Начальная и конечная станции указываются при создании маршрута, а промежуточные могут добавляться между ними.
 	def initialize(first_station, last_station)
-	    @first_station = first_station
-	    @last_station = last_station
+		@first_station = first_station
+		@last_station = last_station
 
-	    @intermediate_stations = []
+		@intermediate_stations = []
 	end
 
-# Может добавлять промежуточную станцию в список
+	# Может добавлять промежуточную станцию в список
 	def add_intermediate_station(station)
 		@intermediate_stations << station
 	end
 
-# Может удалять промежуточную станцию из списка
+	# Может удалять промежуточную станцию из списка
 	def delete_intermediate_station(station)
 		@intermediate_stations.delete(station)
 	end
 
-# Может выводить список всех станций по порядку от начальной до конечной
+	# Может выводить список всех станций по порядку от начальной до конечной
 	def show_route
 		[@first_station] + @intermediate_stations + [@last_station]
 	end
